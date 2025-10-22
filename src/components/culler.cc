d92cb5e0cbcdce20859d3b363d5e4d3e9c1300dd
northstarz <fluffshot@proton.me>
2025-10-22 13:51:06 +0200

    implemented simple viewport culling

--- src/components/culler.cc
+++ src/components/culler.cc
@@ -11,21 +11,35 @@ Culler::Culler(std::shared_ptr<Scene> new_scene_ptr) {
 }
 
 void Culler::cull_scene() {
-
+  
   for(Entity e : m_active_scene->m_loaded_entities) {
-
     for(std::shared_ptr<Mesh> m : e.m_mesh) {
+      cull_distance(m);
+      cull_behind(m);
+    }    
+  }  
+  
+};
 
-      //get distance from cam vs threshold
+void Culler::cull_distance(std::shared_ptr<Mesh> m) {
+  //get distance from cam vs threshold
+  glm::vec3 midpoint_pos = glm::vec3(m->AABB_visualizer->min_corner + m->AABB_visualizer->min_corner) / 2.0f; 
+  if(glm::length(midpoint_pos - m_active_scene->m_local_player->m_player_camera->m_cameraPos) > 20.0f)
+    m->m_mesh_culled = true;
+  else
+    m->m_mesh_culled = false;
+};
 
-      if(glm::length(m->AABB_visualizer->min_corner - m_active_scene->m_local_player->m_player_camera->m_cameraPos) > 20.0f
-	 || glm::length(m->AABB_visualizer->max_corner - m_active_scene->m_local_player->m_player_camera->m_cameraPos) > 20.0f)
-	m->m_mesh_culled = true;
-      else
-	m->m_mesh_culled = false;
+void Culler::cull_behind(std::shared_ptr<Mesh> m) {
+  glm::vec3 midpoint_pos = glm::vec3(m->AABB_visualizer->min_corner + m->AABB_visualizer->min_corner) / 2.0f; 
+  glm::vec3 cam_to_midpoint =  glm::normalize(midpoint_pos - m_active_scene->m_local_player->m_player_camera->m_cameraPos);
+  glm::vec3 cam_to_lookat = glm::normalize(m_active_scene->m_local_player->m_player_camera->m_cameraLookAt);
 
-    }
-    
-  }
+  float angle = glm::dot(cam_to_lookat, cam_to_midpoint);
+  
+  if(angle < 0.0f)
+    m->m_mesh_culled = true;
+  else
+    m->m_mesh_culled = false;
   
 };
--- src/components/culler.hh
+++ src/components/culler.hh
@@ -8,6 +8,9 @@ public:
 
   void cull_scene();
   void update_active_scene(std::shared_ptr<Scene> new_scene);
+
+  void cull_distance(std::shared_ptr<Mesh>);
+  void cull_behind(std::shared_ptr<Mesh>);
   
   Culler(std::shared_ptr<Scene> new_scene);
   
